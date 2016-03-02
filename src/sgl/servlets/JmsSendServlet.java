b2d061862b62bb050af2c890dfe7a1ff2a8b84d1
dizilek21 <dizilek21@gmail.com>
2016-03-02 02:15:00 +0200

    add old files

---
+++ src/sgl/servlets/JmsSendServlet.java
@@ -0,0 +1,58 @@
+package sgl.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.jms.*;        
+import javax.naming.*;    
+
+
+@WebServlet("/JmsSendServlet")
+public class JmsSendServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public JmsSendServlet() {
+        super();
+        
+    }
+
+	
+    
+    
+    
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try {
+		//Context ctx =  
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		catch (JMSException je)
+	    {
+	       System.out.println("JMS failed with "+je);
+	       Exception le = je.getLinkedException();
+	       if (le != null)
+	       {
+	           System.out.println("linked exception "+le);
+	       }
+	    }
+		
+	
+
+}
+	
+}
