d2941a7c407ea5b28bc0e1f41afc0607a77041eb
dizilek21 <dizilek21@gmail.com>
2016-03-09 02:56:12 +0200

    Add first MDB

---
+++ src/sgl/FirstMDB.java
@@ -0,0 +1,34 @@
+package sgl;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ * Message-Driven Bean implementation class for: FirstMDB
+ */
+@MessageDriven(
+		activationConfig = { @ActivationConfigProperty(
+				propertyName = "destination", propertyValue = "jms/Q1"), @ActivationConfigProperty(
+				propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+		}, 
+		mappedName = "jms/Q1")
+public class FirstMDB implements MessageListener {
+
+    /**
+     * Default constructor. 
+     */
+    public FirstMDB() {
+        // TODO Auto-generated constructor stub
+    }
+	
+	/**
+     * @see MessageListener#onMessage(Message)
+     */
+    public void onMessage(Message message) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
