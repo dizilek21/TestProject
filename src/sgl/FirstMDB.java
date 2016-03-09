585989e413c161454e452a99bd9b1fa81e478999
dizilek21 <dizilek21@gmail.com>
2016-03-09 02:58:30 +0200

    commit 1 with firstMDB

--- src/sgl/FirstMDB.java
+++ src/sgl/FirstMDB.java
@@ -27,7 +27,7 @@ public class FirstMDB implements MessageListener {
      * @see MessageListener#onMessage(Message)
      */
     public void onMessage(Message message) {
-        // TODO Auto-generated method stub
+        
         
     }
 
