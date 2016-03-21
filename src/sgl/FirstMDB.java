b5a49ef7fe48462a6f3d885f5da085014a2eef3a
dizilek21 <dizilek21@gmail.com>
2016-03-21 08:03:06 +0200

    jmsSendServlet

--- src/sgl/FirstMDB.java
+++ src/sgl/FirstMDB.java
@@ -1,34 +1,32 @@
-package sgl;
-
-import javax.ejb.ActivationConfigProperty;
-import javax.ejb.MessageDriven;
-import javax.jms.Message;
-import javax.jms.MessageListener;
-
-/**
- * Message-Driven Bean implementation class for: FirstMDB
- */
-@MessageDriven(
-		activationConfig = { @ActivationConfigProperty(
-				propertyName = "destination", propertyValue = "jms/Q1"), @ActivationConfigProperty(
-				propertyName = "destinationType", propertyValue = "javax.jms.Queue")
-		}, 
-		mappedName = "jms/Q1")
-public class FirstMDB implements MessageListener {
-
-    /**
-     * Default constructor. 
-     */
-    public FirstMDB() {
-        // TODO Auto-generated constructor stub
-    }
-	
-	/**
-     * @see MessageListener#onMessage(Message)
-     */
-    public void onMessage(Message message) {
-        
-        
-    }
-
-}
+//package sgl;
+//
+//import javax.ejb.ActivationConfigProperty;
+//import javax.ejb.MessageDriven;
+//import javax.jms.Message;
+//import javax.jms.MessageListener;
+//
+//
+//
+//@MessageDriven(
+//		activationConfig = { @ActivationConfigProperty(
+//				propertyName = "destination", propertyValue = "jms/Q1"), @ActivationConfigProperty(
+//				propertyName = "destinationType", propertyValue = "javax.jms.Queue")}, 
+//		mappedName = "jms/Q1")
+//public class FirstMDB implements MessageListener {
+//
+//    /**
+//     * Default constructor. 
+//     */
+//    public FirstMDB() {
+//       
+//    }
+//	
+//
+//    
+//    public void onMessage(Message message) {
+//        
+//    	
+//        
+//    }
+//
+//}
--- src/sgl/servlets/JmsSendServlet.java
+++ src/sgl/servlets/JmsSendServlet.java
@@ -6,8 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import javax.jms.*;        
-//import javax.naming.*;    
+import javax.jms.*;        
+import javax.naming.*;    
 
 
 @WebServlet("/JmsSendServlet")
@@ -27,26 +27,70 @@ public class JmsSendServlet extends HttpServlet {
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		System.out.println("In DoGet..");
+		QueueConnection queueConnection = null;
+		QueueSession queueSession = null;
+		QueueSender queueSender = null;
+		String message = "";
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-	/*	try {
-		//Context ctx =  
-		}
-		
-		
-			
-		catch (JMSException je)
-	    {
-	       System.out.println("JMS failed with "+je);
-	       Exception le = je.getLinkedException();
-	       if (le != null)
-	       {
-	           System.out.println("linked exception "+le);
-	       }
-	    }
-		
-	*/
+		System.out.println("before message..");
+		message = request.getParameter(message);
+		System.out.println("after message..");
+		System.out.println("message is: " + message);	
+		try {
+				Context ctx = new InitialContext();  
+				QueueConnectionFactory qcf = (QueueConnectionFactory) ctx.lookup("jms/QCF1");
+				Queue queue = (Queue) ctx.lookup("jms/Q1");
 
-}
+				queueConnection = qcf.createQueueConnection();
+				queueConnection.start();
+
+				queueSession = queueConnection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+				queueSender = queueSession.createSender(queue);
+				TextMessage textMessage = queueSession.createTextMessage(message);
+				queueSender.send(textMessage);
+			
+		}	catch (Throwable t) {
+					t.printStackTrace();
+					response.getWriter().println("Error occurred while trying to send message : " + t.toString() + "\n");
+			
+			/*catch (JMSException je){
+					    
+					       System.out.println("JMS failed with "+je);
+					       Exception le = je.getLinkedException();
+					       if (le != null)
+					       {
+					           System.out.println("linked exception "+le);
+					       }*/
+		} finally {
+					try {
+					if (queueSender != null) {
+						queueSender.close();
+					}
+					if (queueSession != null) {
+						queueSession.close();
+					}
+					if (queueConnection != null) {
+						queueConnection.close();
+					}
+					} catch (Throwable t) {
+					}
+				}
+	}
+
+	
+	
+	
+	
 	
 }
+			
+			
+
+	    
+		
+	
+
+
+
